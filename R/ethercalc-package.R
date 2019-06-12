eb75dd450b98c35cc27b426e5311bc5a80ae4a39
hrbrmstr <bob@rud.is>
2019-06-12 08:19:15 -0400

    R package repo initialization complete

---
+++ R/ethercalc-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/ethercalc>
+#' - BugReports: <https://gitlab.com/hrbrmstr/ethercalc/issues>
+#' 
+#' @md
+#' @name ethercalc
+#' @keywords internal
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+"_PACKAGE"
